06576f28d28efc90406ca48452b89943d1731815
momoeigeta <momoe.igeta@force-corp.co.jp>
2021-04-23 18:09:00 +0900

    追加

---
+++ js/if-for.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// let i = 1;
+// if (i === 1) {
+//     console.log("真です");
+// }
+
+let testscore = 80;
+
+if (testscore >= 80) {
+    document.write("合格です！おめでとうございます！");
+    } else if (testscore >= 70) {
+        document.write("惜しい！あと少し！");
+    } else {
+        document.write("不合格です！もう少し勉強しましょう");
+    }
+    
