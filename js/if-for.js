2897bf711080f701b6d4041ea83d39e3766b4747
momoeigeta <momoe.igeta@force-corp.co.jp>
2021-04-23 18:24:09 +0900

    追加

--- js/if-for.js
+++ js/if-for.js
@@ -5,13 +5,18 @@
 //     console.log("真です");
 // }
 
-let testscore = 80;
+// let testscore = 80;
 
-if (testscore >= 80) {
-    document.write("合格です！おめでとうございます！");
-    } else if (testscore >= 70) {
-        document.write("惜しい！あと少し！");
-    } else {
-        document.write("不合格です！もう少し勉強しましょう");
-    }
-    
+// if (testscore >= 80) {
+//     document.write("合格です！おめでとうございます！");
+//     } else if (testscore >= 70) {
+//         document.write("惜しい！あと少し！");
+//     } else {
+//         document.write("不合格です！もう少し勉強しましょう");
+//     }
+
+const namelist = ["田中","山田","林"];
+
+console.log(namelist[0]);
+console.log(namelist[1]);
+console.log(namelist[2]);
