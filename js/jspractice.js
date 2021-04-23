8864417ae6b60a79ab47b74a4f6dc7c36aa6dcab
momoeigeta <momoe.igeta@force-corp.co.jp>
2021-04-23 17:50:37 +0900

    追加

--- js/jspractice.js
+++ js/jspractice.js
@@ -1,1 +1,39 @@
-document.write("Hello world!");
+// document.write("Hello world!");
+// alert('Hello World!');
+
+// console.log('Hello World!' + '田中さん');
+// console.log(10 + 10);
+// console.log('10 + 10');
+// console.log('10' + '10');
+
+// console.log(1 + 1);
+// console.log(1 - 1);
+// console.log(2 * 2);
+// console.log(10 / 3);
+// console.log(10 % 3);
+
+// let x = 1;
+// let y = 1;
+// x++; //x = x + 1
+// console.log(x);
+// y--; //y = y - 1
+// console.log(y);
+
+// let x = 7;
+// let y = x++;
+// let x2 = 7;
+// let y2 = ++x2;
+
+// console.log(x);
+// console.log(y);
+// console.log(x2);
+// console.log(y2);
+
+// console.log(x++);
+// console.log(++x);
+
+let age = 30;
+const name = "田中太郎";
+age = age + 2;
+// name = "山田花子";
+console.log(`${name}さんの年齢は${age}です`);
