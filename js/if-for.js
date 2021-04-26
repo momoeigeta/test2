b9f16489b0e7588054a6dc98e08273e13ec7cb20
momoeigeta <momoe.igeta@force-corp.co.jp>
2021-04-26 15:40:47 +0900

    追加

--- js/if-for.js
+++ js/if-for.js
@@ -5,18 +5,38 @@
 //     console.log("真です");
 // }
 
-// let testscore = 80;
+function scorecheck() {
+    let testscore = document.getElementById(`score`).value
+    if (testscore >= 80) {
+        document.write("合格です！おめでとうございます！");
+        } else if (testscore >= 70) {
+            document.write("惜しい！あと少し！");
+        } else {
+            document.write("不合格です！もう少し勉強しましょう");
+        }
+}
 
-// if (testscore >= 80) {
-//     document.write("合格です！おめでとうございます！");
-//     } else if (testscore >= 70) {
-//         document.write("惜しい！あと少し！");
-//     } else {
-//         document.write("不合格です！もう少し勉強しましょう");
+
+
+// const namelist = ["田中","山田","林","佐藤"];
+
+// console.log(namelist[0]);
+// console.log(namelist[1]);
+// console.log(namelist[2]);
+
+
+// for (let i = 0; i < namelist.length; i++) {
+//     console.log(`${i+1}.${namelist[i]}`);
+// }
+
+// continue文
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) continue;
+//     console.log(i);
 //     }
 
-const namelist = ["田中","山田","林"];
-
-console.log(namelist[0]);
-console.log(namelist[1]);
-console.log(namelist[2]);
+// break文
+// for (let i = 1; i < 1000; i++) {
+//     if (i>10) break;
+//     console.log(i);
+// }
