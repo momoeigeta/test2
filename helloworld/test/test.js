9e7149dccaf857551de1fcacfbf2b96ea68b75f6
momoeigeta <momoe.igeta@force-corp.co.jp>
2021-06-10 09:57:14 +0900

    変更

---
+++ helloworld/test/test.js
@@ -0,0 +1,24 @@
+describe("pow", () => {
+
+    describe("raises x to power n", () => {
+
+        const makeTest = (x) => {
+            let expected = x * x * x;
+            it(`${x} in the power 3 is ${expected}`, () => {
+                assert.equal(pow(x, 3), expected);
+            });
+        }
+
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+        }
+    });
+
+    it("if n is negative, the result is NaN", function () {
+        assert.isNaN(pow(2, -1));
+    });
+
+    it("if n is not integer, the result is NaN", function () {
+        assert.isNaN(pow(2, 1.5));
+    });
+});
